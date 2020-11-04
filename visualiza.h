103ab412985ec35038ac8d78b153d146a3c08582
FilipeJN <73920883+FilipeJN@users.noreply.github.com>
2020-11-03 23:35:01 -0300

    Update visualiza.h

--- visualiza.h
+++ visualiza.h
@@ -1,5 +1,22 @@
+#ifndef VISUALIZA_H
+#define VISUALIZA_H
 
+#include "cadastro.h"
 #include "vendas.h"
 
 /* recebe o estoque e calcula o relatorio */
 void relatoriodeVendas (produto * vendidos);
+
+/* exibe o nome dos produtos e o preço */
+void exibepreco(produto * estoque);
+
+/* exibe um relatorio sobre as vendas */
+void relatorioVendas(produto * vendidos);
+
+/* exibe um relatorio sobre o estoque */
+void relatorioEstoque(produto * estoque);
+
+/* exibe um menu com a opções de relatorio */
+void menuRelatorios(produto * estoque, produto * vendidos);
+
+#endif //VISUALIZA_H

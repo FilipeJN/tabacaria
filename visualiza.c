b43227d5bb0a4cbb8dcfe650f746ff784a9b54ed
FilipeJN <73920883+FilipeJN@users.noreply.github.com>
2020-11-04 00:50:34 -0300

    Update visualiza.c

--- visualiza.c
+++ visualiza.c
@@ -20,35 +20,39 @@ void exibepreco(produto * estoque){
 	int i, n;
 	n=tamanho_estoque();
 	for(i = 0; i<n;i++){
-		printf("Codigo produto: %d ", estoque[i].codigo);
+		printf("Codigo produto: %ld ", estoque[i].codigo);
 		printf("Nome do produto: %s ", estoque[i].nome);
 		printf("Preço do produto: %.2f\n", estoque[i].preco_venda);
 	} 
 }
 
 void relatorioEstoque(produto * estoque){
-		
-	printf("\n *** Produtos atualmente em estoque:\n");
+	if (tamanho_estoque()	== 0){
+    printf("Sem produtos no estoque.\n");
+  }else{
+    printf("\n *** Produtos atualmente em estoque:\n");
     imprime_estoque(estoque);
-    	
+        
     printf("Preço dos produtos:\n");
-	int i;
-	for(i=0; i<tamanho_estoque(); i++);{
-		printf("R$ %.2f\n", estoque[i].preco_venda);	
-	}
+    int i;
+    for(i=0; i<tamanho_estoque(); i++);{
+      printf("R$ %.2f\n", estoque[i].preco_venda);	
+    }
+  }
 }
 
 void menuRelatorios(produto * estoque, produto * vendidos){
 	int opc;
 	do{
 		printf("Escolha uma das Opções:\n");
-		printf("1 - Relatorio de vendas.");
-		printf("2 - Relatorio de estoque.");		
-		printf("3 - Sair.");
+		printf("1 - Relatorio de vendas.\n");
+		printf("2 - Relatorio de estoque.\n");		
+		printf("3 - Sair.\n");
+    		scanf("%d", &opc);
 		
 		switch(opc){
 			case 1:
-				relatorioVendas(vendidos);
+				relatoriodeVendas(vendidos);
 			break;
 			
 			case 2:
@@ -63,4 +67,3 @@ void menuRelatorios(produto * estoque, produto * vendidos){
 		}
 	}while (opc !=3);	
 }
-
